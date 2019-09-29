7ab739c9bcace359c7d75b0417725c19d17b1084
Tito <tito.parra4@@hotmail.com>
2019-09-29 03:00:20 +0200

    Create DataBase

---
+++ MyVet.Web/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVet.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Agenda> Agendas { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetType> PetTypes { get; set; }
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
+    }
+}
