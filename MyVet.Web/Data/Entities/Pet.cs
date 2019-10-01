59bca8a3eeb2c65b1a56fc6c88486ce62d44ae94
Tito <tito.parra4@@hotmail.com>
2019-10-01 18:42:24 +0200

    Publish on Azure

--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -29,7 +29,7 @@ namespace MyVet.Web.Data.Entities
         //TODO: replace the correct URL for the image
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
             ? null
-            : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+            : $"https://myvettito.azurewebsites.net{ImageUrl.Substring(1)}";
 
         [Display(Name = "Born")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
