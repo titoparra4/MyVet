f7c233bbbea2a4ae5887a70a2738737d3d1fc692
Tito <tito.parra4@@hotmail.com>
2019-10-05 19:10:24 +0200

    API for recover password

--- MyVet.Web/Controllers/API/AccountController.cs
+++ MyVet.Web/Controllers/API/AccountController.cs
@@ -88,5 +88,42 @@ namespace MyVet.Web.Controllers.API
                 Message = "A Confirmation email was sent. Please confirm your account and log into the App."
             });
         }
+
+        [HttpPost]
+        [Route("RecoverPassword")]
+        public async Task<IActionResult> RecoverPassword([FromBody] EmailRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "Bad request"
+                });
+            }
+
+            var user = await _userHelper.GetUserByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "This email is not assigned to any user."
+                });
+            }
+
+            var myToken = await _userHelper.GeneratePasswordResetTokenAsync(user);
+            var link = Url.Action("ResetPassword", "Account", new { token = myToken }, protocol: HttpContext.Request.Scheme);
+            _mailHelper.SendMail(request.Email, "Password Reset", $"<h1>Recover Password</h1>" +
+                $"To reset the password click in this link:</br></br>" +
+                $"<a href = \"{link}\">Reset Password</a>");
+
+            return Ok(new Response<object>
+            {
+                IsSuccess = true,
+                Message = "An email with instructions to change the password was sent."
+            });
+        }
+
     }
 }
