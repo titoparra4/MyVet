56cc039b8c70605a6fe99792f4bc4e777ece50ae
Tito <tito.parra4@@hotmail.com>
2019-10-05 14:07:13 +0200

    Recover Password

--- MyVet.Web/Helpers/IUserHelper.cs
+++ MyVet.Web/Helpers/IUserHelper.cs
@@ -35,6 +35,11 @@ namespace MyVet.Web.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+
     }
 }
 
