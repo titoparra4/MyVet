83a8d5eedd9c2a3720d6751b765c558168fe5618
Tito <tito.parra4@@hotmail.com>
2019-10-16 13:26:06 +0200

    Move map to current position

---
+++ MyVet.Common/Services/IGeolocatorService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace MyVet.Common.Services
+{
+    public interface IGeolocatorService
+    {
+        double Latitude { get; set; }
+
+        double Longitude { get; set; }
+
+        Task GetLocationAsync();
+    }
+}
+
--- MyVet.Prism/MyVet.Prism/Views/MapPage.xaml.cs
+++ MyVet.Prism/MyVet.Prism/Views/MapPage.xaml.cs
@@ -1,12 +1,31 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using MyVet.Common.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace MyVet.Prism.Views
 {
     public partial class MapPage : ContentPage
     {
-        public MapPage()
+        private readonly IGeolocatorService _geolocatorService;
+
+        public MapPage(IGeolocatorService geolocatorService)
         {
             InitializeComponent();
+            _geolocatorService = geolocatorService;
+            MoveMapToCurrentPositionAsync();
         }
+
+        private async void MoveMapToCurrentPositionAsync()
+        {
+            await _geolocatorService.GetLocationAsync();            
+                var position = new Position(
+                    _geolocatorService.Latitude,
+                    _geolocatorService.Longitude);
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                    position,
+                    Distance.FromKilometers(.5)));            
+        }
+
     }
 }
